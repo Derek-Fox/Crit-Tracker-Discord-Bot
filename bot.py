e1ec5dbe74ca6c21f930b94f4ad38e6542bbe953
Derek Fox <derekefox@gmail.com>
2023-02-15 15:40:31 -0500

    Bot now connects to guild and displays guild ID

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client(command_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+        
+    print(
+        f'{client.user} has connected to guild: \n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    
+client.run(TOKEN)
+
+
+
