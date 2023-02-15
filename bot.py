a436fbc87f941f3a1a3d2bee114f06fedee47503
Derek Fox <derekefox@gmail.com>
2023-02-15 16:17:37 -0500

    Changed to bot impl. and added add cmd

--- bot.py
+++ bot.py
@@ -2,26 +2,26 @@
 import os
 import random
 
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+
+intents = discord.Intents(messages=True, guilds=True)
+intents.message_content = True
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.command(name='99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
+@bot.command(name='add')
+async def add(ctx, crit_type):
 
-    response = random.choice(brooklyn_99_quotes)
+    if crit_type == '20':
+        response = '20 added'
+    elif crit_type == '1':
+        response = '1 added'
+        
     await ctx.send(response)
 
 bot.run(TOKEN)
