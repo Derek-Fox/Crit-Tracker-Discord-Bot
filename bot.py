bdca345269997fb5509de1284ab6eb4f004b6ff4
Derek Fox <derekefox@gmail.com>
2023-03-01 12:57:37 -0500

    refactored. should make more sense now.

--- bot.py
+++ bot.py
@@ -2,10 +2,8 @@ from __future__ import print_function
 
 import os
 import os.path
-import random
 
 import discord
-import google.auth
 from discord.ext import commands
 from dotenv import load_dotenv
 from google.auth.transport.requests import Request
@@ -14,7 +12,31 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+SHEET_ID = os.getenv('SHEET_ID')
+
+INTENTS = discord.Intents(messages=True, guilds=True)
+INTENTS.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=INTENTS)
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+CREDS = None
+if os.path.exists('token.json'):
+    CREDS = Credentials.from_authorized_user_file('token.json', SCOPES)
+# If there are no (valid) credentials available, let the user log in.
+if not CREDS or not CREDS.valid:
+    if CREDS and CREDS.expired and CREDS.refresh_token:
+        CREDS.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        CREDS = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', mode='w', encoding='UTF-8') as token:
+        token.write(CREDS.to_json())
+
 
 def update_values(spreadsheet_id, range_name, value_input_option, _values, creds):
     try:
@@ -30,7 +52,8 @@ def update_values(spreadsheet_id, range_name, value_input_option, _values, creds
     except HttpError as error:
         print(f"An error occurred: {error}")
         return error
-    
+
+
 def get_values(spreadsheet_id, range_name, creds):
     try:
         service = build('sheets', 'v4', credentials=creds)
@@ -42,43 +65,22 @@ def get_values(spreadsheet_id, range_name, creds):
     except HttpError as error:
         print(f"An error occurred: {error}")
         return error
-    
+
+
 def get_and_update(cell, creds):
     value = get_values(SHEET_ID, cell, creds).get('values', [])
-    
-    update_values(SHEET_ID, cell, "USER_ENTERED", [[int(value[0][0]) + 1]], creds)
-    
+
+    update_values(SHEET_ID, cell, "USER_ENTERED",
+                  [[int(value[0][0]) + 1]], creds)
+
     print(f"{cell} updated!")
-    
+
     return int(value[0][0]) + 1
 
-intents = discord.Intents(messages=True, guilds=True)
-intents.message_content = True
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-SHEET_ID = os.getenv('SHEET_ID')
-
-bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.command(name='add')
 async def add(ctx, crit_type, char_name):
-    creds = None
-   
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
 
-    worked = True
     cell = ""
     match char_name.upper():
         case "ZOHAR":
@@ -96,22 +98,22 @@ async def add(ctx, crit_type, char_name):
         case "OATMEAL":
             cell = "10"
         case _:
-            response = "Please enter a valid character name!"
-            worked = False
-    if worked:  
-        if crit_type == '20':
-            cell = "B" + cell
-            response = '20 added! B)'
-        elif crit_type == '1':
-            cell = "C" + cell
-            response = '1 added :('
-        else:
-            response = "Please enter a valid crit type!"
-            worked = False
-    if worked:
-        num_crits = get_and_update(cell, creds)
-        response = f"{response}\n{char_name} now has {num_crits} {crit_type}s!"
-        
+            await ctx.send("Please enter a valid character name!")
+            return
+
+    if crit_type == '20':
+        cell = "B" + cell
+        response = '20 added! B)'
+    elif crit_type == '1':
+        cell = "C" + cell
+        response = '1 added :('
+    else:
+        await ctx.send("Please enter a valid crit type!")
+        return
+
+    num_crits = get_and_update(cell, CREDS)
+    response = f"{response}\n{char_name} now has {num_crits} {crit_type}s!"
+
     await ctx.send(response)
 
 bot.run(TOKEN)
