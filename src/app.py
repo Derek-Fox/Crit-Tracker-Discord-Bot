78a50f2a5bdbec7964a8929c22a4dc5e733edcad
Derek Fox <derekefox@gmail.com>
2025-05-04 10:50:05 -0400

    Forgor

---
+++ src/app.py
@@ -0,0 +1,99 @@
+import json
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+import colorlog
+from dotenv import load_dotenv
+from os import getenv
+import google.generativeai as genai
+from google.auth.exceptions import RefreshError
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from sheets import SheetsHandler
+from bot import Tim
+
+
+def config_logging():
+    LOG_FILE = "./out.log"
+    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+    LOG_LEVEL = logging.DEBUG
+
+    file_handler = RotatingFileHandler(LOG_FILE, maxBytes=1_000_000, backupCount=3)
+    file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+
+    console_handler = colorlog.StreamHandler()
+    console_formatter = colorlog.ColoredFormatter(
+        "%(log_color)s%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red",
+        },
+    )
+    console_handler.setFormatter(console_formatter)
+
+    logging.basicConfig(level=LOG_LEVEL, handlers=[file_handler, console_handler])
+
+    discord_logger = logging.getLogger("discord")
+    discord_logger.propagate = False
+
+
+def load_env() -> dict:
+    load_dotenv()
+    env = {
+        "TOKEN": getenv("DISCORD_TOKEN"),
+        "SHEET_ID": getenv("SHEET_ID"),
+        "PWSH_PATH": rf"{getenv('PWSH_PATH')}",
+        "GEMINI_KEY": getenv("GEMINI_API_KEY"),
+    }
+
+    for key, value in env.items():
+        if not value:
+            logging.error(f"Env var '{key}' not found. Exiting...")
+            exit(1)
+
+    return env
+
+
+def init_model(env: dict):
+    with open("./genai/tim_config.json") as f:
+        tim_config = json.load(f)
+
+    try:
+        genai.configure(api_key=env["GEMINI_KEY"])
+        tim = genai.GenerativeModel(
+            model_name=tim_config["model_name"],
+            system_instruction=tim_config["instruction"],
+            generation_config=genai.GenerationConfig(
+                temperature=tim_config["temperature"]
+            ),
+        )
+        tim_chat = tim.start_chat()
+        logging.info("Tim genai model initialized successfully")
+    except Exception as e:
+        logging.error(f"Exiting, failed to initialize tim: {e}")
+        exit(1)
+
+    return tim_chat
+
+
+def main():
+    config_logging()
+
+    env = load_env()
+
+    tim_chat = init_model(env)
+
+    sheets = SheetsHandler(env['SHEET_ID'])
+    
+    bot = Tim()
+
+    bot.run(os.getenv("DISCORD_TOKEN"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/sheets.py
@@ -0,0 +1,103 @@
+import os
+import logging
+
+from google.auth.exceptions import RefreshError
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+class SheetsHandler:
+
+    def __init__(self, sheet_id):
+        self.sheet_id = sheet_id
+        scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+        self.creds = None
+        if os.path.exists("token.json"):
+            logging.info("Found token.json. Attempting to load credentials...")
+            self.creds = Credentials.from_authorized_user_file("token.json", scopes)
+        else:
+            logging.warning("token.json not found. Beginning authentication...")
+
+        if not self.creds or not self.creds.valid:
+            try:
+                if self.creds and self.creds.expired and self.creds.refresh_token:
+                    logging.info("Refreshing credentials...")
+                    self.creds.refresh(Request())
+                else:
+                    logging.warning("No valid credentials found. Starting oAuth flow...")
+                    raise RefreshError
+            except RefreshError:
+                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", scopes)
+                flow.authorization_url(access_type="offline", include_granted_scopes="true")
+                self.creds = flow.run_local_server(port=0)
+                logging.info("OAuth flow completed successfully.")
+            with open("token.json", "w", encoding="UTF-8") as token:
+                token.write(self.creds.to_json())
+                logging.info("Credentials saved to token.json")
+
+
+    def update_values(self, spreadsheet_id, subsheet_id, range_name, value_input_option, _values):
+        """Updates values on the spreadsheet in the given range with given values"""
+        range_name = f"{subsheet_id}!{range_name}"
+        try:
+            service = build("sheets", "v4", credentials=self.creds)
+            body = {"values": _values}
+            # pylint: disable=maybe-no-member
+            result = (
+                service.spreadsheets()
+                .values()
+                .update(
+                    spreadsheetId=spreadsheet_id,
+                    range=range_name,
+                    valueInputOption=value_input_option,
+                    body=body,
+                )
+                .execute()
+            )
+            logging.info(
+                f"Updated {result.get('updatedCells')} cells in range '{range_name}' on sheet '{subsheet_id}'."
+            )
+            return result
+        except HttpError as error:
+            logging.error(
+                f"Failed to update values in range '{range_name}' on sheet '{subsheet_id}': {error}"
+            )
+            return error
+
+
+    def get_values(self, spreadsheet_id, subsheet_id, range_name):
+        """Returns values from the spreadsheet from the specified range"""
+        range_name = f"{subsheet_id}!{range_name}"
+        try:
+            service = build("sheets", "v4", credentials=self.creds)
+            logging.info(
+                f"Attempting to retrieve values from range '{range_name}' on sheet '{subsheet_id}'..."
+            )
+            result = (
+                service.spreadsheets()
+                .values()
+                .get(spreadsheetId=spreadsheet_id, range=range_name)
+                .execute()
+            )
+            rows = result.get("values", [])
+            logging.info(
+                f"Successfully retrieved {len(rows)} rows from range '{range_name}' on sheet '{subsheet_id}'."
+            )
+            return result
+        except HttpError as error:
+            logging.error(
+                f"Failed to retrieve values from range '{range_name}' on sheet '{subsheet_id}': {error}"
+            )
+            return error
+
+
+    def get_and_update(self, cell, subsheet_id):
+        """Increments the value of the given cell by 1."""
+        value = self.get_values(self.sheet_id, subsheet_id, cell).get("values", [])
+
+        self.pdate_values(self.sheet_id, subsheet_id, cell, "USER_ENTERED", [[int(value[0][0]) + 1]])
+
+        return int(value[0][0]) + 1
