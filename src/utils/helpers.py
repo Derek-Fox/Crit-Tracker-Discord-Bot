ad93de1265dda602440ac8d5959f12e76510a8a0
Derek Fox <derekefox@gmail.com>
2026-04-08 20:41:21 -0400

    Update util functions to work with new slash commands

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,11 +1,12 @@
-'''
+"""
 Contains helper functions for the bot, such as formatting messages and sending error embeds.
-'''
+"""
 
 import logging
 import subprocess
 import platform
 import discord
+from discord import Interaction
 from discord import FFmpegPCMAudio
 
 
@@ -13,6 +14,7 @@ def cow_format(message: str | None, pwsh_path: str) -> str:
     """
     Formats a message as a cow saying it using the cowsay subprocess.
     :param message: The message to format.
+    :param pwsh_path: The path to the pwsh executable.
     :return: The formatted message.
     """
     if not message:
@@ -20,7 +22,6 @@ def cow_format(message: str | None, pwsh_path: str) -> str:
         message = "* The cow stares at you blankly *"
 
     logging.info("Running cowsay command with message=%s.", message)
-
 
     if platform.system() == "Windows":
         logging.info(
@@ -45,6 +46,7 @@ def talk_to_tim(message: str, name: str, tim_chat) -> str:
     Sends a message to Tim and returns his response.
     :param message: The message to send to Tim.
     :param name: The name of the person sending the message.
+    :tim_chat: The Tim chat object.
     :return: Tim's response text with newline stripped.
     """
     logging.info("Sending message to Tim: '%s' from user '%s'.", message, name)
@@ -59,39 +61,41 @@ def talk_to_tim(message: str, name: str, tim_chat) -> str:
 
 
 async def send_error_embed(inter: discord.Interaction, message):
-    '''
+    """
     Sends an error embed to the given context with the given message.
-    
-    :param ctx: The context in which the error occurred.
+
+    :param inter: The interaction in which the error occurred.
     :param message: The error message to display.
-    '''
+    """
     embed = discord.Embed(
         title="**Error**", description=message, color=discord.Color.red()
     )
     embed.set_thumbnail(url="attachment://warning.png")
-    await inter.response.send_message(file=discord.File("res/warning.png"), embed=embed)
+    try:
+        await inter.response.send_message(file=discord.File("res/warning.png"), embed=embed)
+    except discord.InteractionResponded:
+        await inter.followup.send(file=discord.File("res/warning.png"), embed=embed)
 
 
-def play_sound(ctx, sound):
-    '''
+def play_sound(inter: discord.Interaction, sound):
+    """
     Plays a sound in the given context's voice channel.
-    
-    :param ctx: The context in which to play the sound.
+
+    :param inter: The interaction which asked to play the sound.
     :param sound: The path to the sound file to play.
-    '''
-    if ctx.voice_client:
-        voice = ctx.guild.voice_client
+    """
+    if inter.user.voice:
         source = FFmpegPCMAudio(sound)
-        voice.play(source)
+        inter.guild.voice_client.play(source)
         logging.info("Sound '%s' played in channel '%s'.", sound, voice.channel)
 
 
-def get_msg_author_name(ctx):
-    '''
+def get_msg_author_name(inter):
+    """
     Returns the display name of the author of a message in a given context.
-    
-    :param ctx: The context from which to extract the author's name.
-    '''
-    return ctx.message.author.display_name.partition("(")[
+
+    :param inter: The interaction from which to extract the author's name.
+    """
+    return inter.user.display_name.partition("(")[
         0
-    ]  # names in this server are formatted as "name (nickname)"
+    ].strip()  # names in this server are formatted as "name (nickname)"
